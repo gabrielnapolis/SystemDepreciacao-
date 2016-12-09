ed733a0303a6e50190574e229e329f926fa8897a
Aluno <Aluno@PC-42229>
2016-12-09 11:27:04 -0200

    extração datas

--- SystemDepreciacao/src/main/java/br/com/sysdepre/model/item/ItemRN.java
+++ SystemDepreciacao/src/main/java/br/com/sysdepre/model/item/ItemRN.java
@@ -19,12 +19,33 @@ public class ItemRN implements Serializable{
 	
 	public int CalcularPeriodo(Item i){
 			int qntAnos = 0;
+			
+			//Conversão de Datas para String
 			String dataCompra = i.getDataAquisicao().toString();
 			String dataVenda = i.getDataVenda().toString();
 			
+			//Extração do Dia
+			String diaCompra = dataCompra.substring(0,2);
+			String diaVenda  = dataVenda.substring(0,2);
 			
+			//Extração do Mes
+			String mesCompra = dataCompra.substring(3,5);
+			String mesVenda  = dataCompra.substring(3,5);
 			
+			//Extração do Ano
+			String anoCompra = dataCompra.substring(6,8);
+			String anoVenda  = dataVenda.substring(6,8);
 			
+			System.out.println("Data Compra: "+dataCompra);
+			System.out.println("Dia Compra: "+diaCompra);
+			System.out.println("Mes compra: "+mesCompra);
+			System.out.println("Ano compra: "+anoCompra);
+			
+				
+			System.out.println("Data Venda: "+dataVenda);
+			System.out.println("Mes Venda: "+diaVenda);
+			System.out.println("Dia Venda: "+mesVenda);
+			System.out.println("Ano Venda: "+anoVenda);
 			
 			return qntAnos;
 	}
